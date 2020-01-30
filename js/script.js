1fe2ce74aef0e68ad8331402ba4cd658993146ab
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-30 15:58:12 +0100

    Impostazione chiamata Ajax. Funziona!

--- js/script.js
+++ js/script.js
@@ -7,7 +7,18 @@
 $(document).ready(function () {
   // Ad ogni click del quadrato deve partire una richiesta AJAX per prendere un numero da 1 a 9. Quando mi viene fornito un risultato, devo effettuare un confronto per capire se il numero è minore di 5 o maggiore di 5. Se minore di 5 il quadrato diventerà giallo, se maggiore di 5 diventerà verde.
   $('.quadrato').click(function () {
-    console.log('provaClick');
-  });
+    // console.log('provaClick');
+    $.ajax( {
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (datoDelClick, stato) {
+        console.log(datoDelClick.response);
+      },
+      error: function (datoDelClick, stato) {
+        alert("E' avvenuto un errore.");
+      }
+    }
+  );
+});
 
 });
