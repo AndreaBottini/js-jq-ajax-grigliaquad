352752a807664fbe5ba9ae9c0f194de6f2c65f42
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-30 17:01:56 +0100

    Risoluzione esercizio

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@
 
 // Inizializzo il documento
 $(document).ready(function () {
-
   // Ad ogni click del quadrato deve partire una richiesta AJAX per prendere un numero da 1 a 9. Quando mi viene fornito un risultato, devo effettuare un confronto per capire se il numero è minore di 5 o maggiore di 5. Se minore di 5 il quadrato diventerà giallo, se maggiore di 5 diventerà verde.
   $('.quadrato').click(function () {
     // console.log('provaClick');
+    var selezione = $(this);
     $.ajax( {
       url: "https://flynn.boolean.careers/exercises/api/random/int",
       method: "GET",
       success: function (datoDelClick, stato) {
+        gioco(datoDelClick.response);
         console.log(datoDelClick.response);
       },
       error: function (datoDelClick, stato) {
@@ -21,11 +22,13 @@ $(document).ready(function () {
     }
   );
   //Effettuo il confronto
-  var risultato = 0;
-  if (risultato <= 5) {
-    console.log(risultato);
-  } else {
-    console.log(risultato);
+  function gioco(risultato) {
+    if (risultato <= 5) {
+      selezione.addClass('yellow');
+    } else {
+      selezione.addClass('green');
+    }
+    selezione.text(risultato)
   }
 });
 
