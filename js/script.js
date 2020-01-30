0b10ab40d2206e7645c58ba9ef93486c16557bc6
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-30 15:19:58 +0100

    Inizializzazione script e analisi esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Griglia 6x6, ad ogni click sul quadrato parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+
+// Inizializzo il documento
+$(document).ready(function () {
+  // Ad ogni click del quadrato deve partire una richiesta AJAX per prendere un numero da 1 a 9. Quando mi viene fornito un risultato, devo effettuare un confronto per capire se il numero è minore di 5 o maggiore di 5. Se minore di 5 il quadrato diventerà giallo, se maggiore di 5 diventerà verde.
+});
